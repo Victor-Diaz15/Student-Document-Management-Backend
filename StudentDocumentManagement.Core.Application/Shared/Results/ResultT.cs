89f17603001ac084b8b9594579dbf7b6aed8e4a7
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-18 17:29:09 -0400

    Agregar clases para el patron result

---
+++ StudentDocumentManagement.Core.Application/Shared/Results/ResultT.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace StudentDocumentManagement.Core.Application.Shared.Results;
+
+public class ResultT<TValue> : Result
+{
+    public TValue? Data { get; set; }
+
+    public ResultT(TValue value)
+        :base() => Data = value;
+
+    public ResultT(bool success, TValue value)
+        : base(success) => Data = value;
+
+    public ResultT(bool success, string message, TValue value)
+        : base(success, message) => Data = value;
+}
+
