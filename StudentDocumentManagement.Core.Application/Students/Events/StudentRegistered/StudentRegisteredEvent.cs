7a587ee391f7c942ec613579a85da6ec8864c4f0
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-18 19:46:42 -0400

    Agregar event y event handler del registro de estudiantes

---
+++ StudentDocumentManagement.Core.Application/Students/Events/StudentRegistered/StudentRegisteredEvent.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace StudentDocumentManagement.Core.Application.Students.Events.StudentRegistered;
+
+public sealed record StudentRegisteredEvent(string Email, string Username, string Password) : INotification;
---
+++ StudentDocumentManagement.Core.Application/Students/Events/StudentRegistered/StudentRegisteredEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using StudentDocumentManagement.Core.Application.Dtos.Emails;
+using StudentDocumentManagement.Core.Application.Interfaces;
+
+namespace StudentDocumentManagement.Core.Application.Students.Events.StudentRegistered;
+
+internal class StudentRegisteredEventHandler : INotificationHandler<StudentRegisteredEvent>
+{
+    private readonly IEmailService _emailService;
+
+    public StudentRegisteredEventHandler(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    public async Task Handle(StudentRegisteredEvent notification, CancellationToken cancellationToken)
+    {
+        var subject = "Bienvenido a Student Document Management App";
+        var body = $"Se ha creado su cuenta con exito, ahora solo debe acceder con su usuario { notification.Username } " +
+            $"y su contraseña { notification.Password }.";
+        var emailRequest = new EmailRequestDto(notification.Email,
+            subject, body);
+
+        await _emailService.SendAsync(emailRequest);
+    }
+}
