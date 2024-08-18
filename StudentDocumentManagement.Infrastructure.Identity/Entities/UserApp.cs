9abbf7ad1ea2c724f67ca7cee879ca5c8a0df103
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-18 17:25:06 -0400

    Agregar entidades del user app y estudiante

---
+++ StudentDocumentManagement.Infrastructure.Identity/Entities/UserApp.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StudentDocumentManagement.Infrastructure.Identity.Entities;
+
+public class UserApp : IdentityUser
+{
+    public string IdentityCard { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Rol { get; set; }
+    public string ProfilePicture { get; set; } = string.Empty;
+}
