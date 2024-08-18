5c9a4d3e5176029576c2f85a69be9363e845a226
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-18 17:31:21 -0400

    Agregar controlador de account

---
+++ StudentDocumentManagement.Api/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StudentDocumentManagement.Core.Application.Accounts.Commands.DeleteAccount;
+using StudentDocumentManagement.Core.Application.Accounts.Commands.Login;
+
+namespace StudentDocumentManagement.Api.Controllers;
+
+[Route("api/account")]
+public class AccountController : ApiController
+{
+    public AccountController(ISender sender) : base(sender)
+    {
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginCommand loginCommand)
+    {
+        var result = await Sender.Send(loginCommand);
+
+        return result.Success ? Ok(result) : BadRequest(new { result.Success, result.Message });
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAccount(string id)
+    {
+        var command = new DeleteAccountCommand(id);
+        var result = await Sender.Send(command);
+
+        return result.Success ? NoContent() : BadRequest(result);
+    }
+   
+}
