412c66792f56b1bcd2d88e7ec5b8faf962dc1f80
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-18 17:31:31 -0400

    Agregar controlador de estudiante

---
+++ StudentDocumentManagement.Api/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StudentDocumentManagement.Core.Application.Students.Commands.RegisterStudent;
+using StudentDocumentManagement.Core.Application.Students.Queries.GetAll;
+using StudentDocumentManagement.Core.Application.Students.Queries.GetById;
+using StudentDocumentManagement.Core.Application.Students.Queries.GetByStudentId;
+using StudentDocumentManagement.Core.Application.Users.Commands.RegisterUser;
+using StudentDocumentManagement.Core.Application.Users.Queries.GetById;
+
+namespace StudentDocumentManagement.Api.Controllers;
+
+[Route("api/student")]
+public class StudentController : ApiController
+{
+    public StudentController(ISender sender) : base(sender)
+    {
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> GetAllStudents()
+    {
+        var query = new GetAllStudentsQuery();
+        var result = await Sender.Send(query);
+
+        return result.Success ? Ok(result) : NotFound(new { result.Success, result.Message });
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetStudentById(string id)
+    {
+        var query = new GetStudentByIdQuery(id);
+        var result = await Sender.Send(query);
+
+        return result.Success ? Ok(result) : NotFound(new { result.Success, result.Message });
+    }
+
+    [HttpGet("studentId/{studentId}")]
+    public async Task<IActionResult> GetStudentByStudentId(string studentId)
+    {
+        var query = new GetStudentByStudentIdQuery(studentId);
+        var result = await Sender.Send(query);
+
+        return result.Success ? Ok(result) : NotFound(new { result.Success, result.Message });
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterStudent([FromBody] RegisterStudentCommand command)
+    {
+        var result = await Sender.Send(command);
+
+        return result.Success ? NoContent() : BadRequest(result);
+    }
+}
