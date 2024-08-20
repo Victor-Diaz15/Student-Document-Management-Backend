94dfdc87fab6ef729ddc6100be1e71e841521b69
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-19 22:14:42 -0400

    Agregar los command validators y el registro en la clase de extension de injeccion de dependencias

---
+++ StudentDocumentManagement.Core.Application/Accounts/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace StudentDocumentManagement.Core.Application.Accounts.Commands.Login;
+
+public class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty();
+
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
