e9195c4f8dfc97447c6e99433e834a7e87682889
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-19 22:15:00 -0400

    esto iba con el commit anterior xd

---
+++ StudentDocumentManagement.Core.Application/Students/Commands/RegisterStudent/RegisterStudentCommandValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+
+namespace StudentDocumentManagement.Core.Application.Students.Commands.RegisterStudent;
+
+public class RegisterStudentCommandValidator : AbstractValidator<RegisterStudentCommand>
+{
+    public RegisterStudentCommandValidator()
+    {
+        RuleFor(x => x.IdentityCard)
+            .NotEmpty()
+            .Length(11);
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .Length(10);
+
+        RuleFor(x => x.Rol)
+            .NotEmpty();
+        
+    }
+}
