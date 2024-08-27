a8794cd48a65bdefd01722122e934e42ab85ace2
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-26 23:05:14 -0400

    Agregar helper del validator del campo de archivo

---
+++ StudentDocumentManagement.Core.Application/Helpers/FileValidationHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace StudentDocumentManagement.Core.Application.Helpers;
+
+public static class FileValidationHelper
+{
+    public static bool BeAValidFileType(IFormFile file, string[] permittedExtensions)
+    {
+        var extension = Path.GetExtension(file.FileName);
+        return permittedExtensions.Contains(extension.ToLower());
+    }
+
+    public static bool BeAValidFileSize(IFormFile file, long maxFileSize)
+    {
+        return file.Length <= maxFileSize;
+    }
+}
