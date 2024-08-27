e7daa7ec2ebda6d64dae0f711703f9a492b2cdee
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-26 23:03:58 -0400

    Agregar clase de mapeo de la configuracion de la subida de archivos

---
+++ StudentDocumentManagement.Core.Domain/Settings/FileUploadSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace StudentDocumentManagement.Core.Domain.Settings;
+
+public class FileUploadSettings
+{
+    public string[] PermittedExtensions { get; set; } = [];
+    public long MaxFileSize { get; set; }
+}
