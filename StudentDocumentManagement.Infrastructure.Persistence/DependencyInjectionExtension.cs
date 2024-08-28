8325b7490ad46a3b0bc98fbe77f34a82f409ef58
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-08-27 22:24:45 -0400

    Agregar dependencias y registrar los servicios

---
+++ StudentDocumentManagement.Infrastructure.Persistence/DependencyInjectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StudentDocumentManagement.Core.Domain.Abstractions;
+using StudentDocumentManagement.Infrastructure.Persistence.Contexts;
+using StudentDocumentManagement.Infrastructure.Persistence.Repositories;
+
+namespace StudentDocumentManagement.Infrastructure.Persistence;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddInfrastructurePersistence(this IServiceCollection services, IConfiguration config)
+    {
+        //agregando el contexto de base de datos y la cadena de conexión
+        services.AddDbContext<MainDbContext>(opts =>
+            opts.UseSqlServer(config.GetConnectionString("defaultConnection"),
+                m => m.MigrationsAssembly(typeof(MainDbContext).Assembly.FullName)
+            ));
+
+        //Repositories
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericBaseRepository<>));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
