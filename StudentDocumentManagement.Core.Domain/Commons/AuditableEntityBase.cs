1905619b5bb46e19111c711eb32b7143918e62c5
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-01 20:19:43 -0400

    Agregar auditable base entity

---
+++ StudentDocumentManagement.Core.Domain/Commons/AuditableEntityBase.cs
@@ -0,0 +1,9 @@
+﻿using StudentDocumentManagement.Core.Domain.Abstractions;
+
+namespace StudentDocumentManagement.Core.Domain.Commons;
+
+public abstract class AuditableEntityBase : IEntityBase
+{
+    public Guid Id { get; set; }
+    public bool Borrado { get; set; }
+}
