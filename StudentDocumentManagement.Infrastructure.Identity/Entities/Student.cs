b8acc303a27d5dbf1d9336547f57478c258839f1
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-01 20:23:00 -0400

    Agregar relacion de los archivos y las solicitudes

--- StudentDocumentManagement.Infrastructure.Identity/Entities/Student.cs
+++ StudentDocumentManagement.Infrastructure.Identity/Entities/Student.cs
@@ -1,6 +1,11 @@
-﻿namespace StudentDocumentManagement.Infrastructure.Identity.Entities;
+﻿using StudentDocumentManagement.Core.Domain.Entities;
+
+namespace StudentDocumentManagement.Infrastructure.Identity.Entities;
 
 public class Student : UserApp
 {
     public string StudentId { get; set; } = string.Empty;
+    public List<StudentFile> Files { get; set; } = [];
+    public List<Application> Applications { get; set; } = [];
+
 }
