bc181043fd0bca1954eb342d3111cbc8a3cf2cc7
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-01 21:31:16 -0400

    Agregar configuraciones de las entidades

---
+++ StudentDocumentManagement.Infrastructure.Persistence/EntityConfigurations/ApplicationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentDocumentManagement.Core.Domain.Entities;
+
+namespace StudentDocumentManagement.Infrastructure.Persistence.EntityConfigurations;
+
+public class ApplicationConfiguration : IEntityTypeConfiguration<Application>
+{
+    public void Configure(EntityTypeBuilder<Application> builder)
+    {
+        builder.ToTable("Applications");
+        builder.HasKey(x => x.Id);
+
+        //Property Configurations
+        builder.Property(x => x.Id).HasColumnName("ApplicationId");
+
+        //Global Filter
+        builder.HasQueryFilter(x => !x.Borrado);
+
+        //Relations
+        builder.HasOne(x => x.Service)
+            .WithMany(x => x.Applications)
+            .HasForeignKey(x => x.ServiceId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ StudentDocumentManagement.Infrastructure.Persistence/EntityConfigurations/ApplicationStudentFileConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentDocumentManagement.Core.Domain.Entities;
+
+namespace StudentDocumentManagement.Infrastructure.Persistence.EntityConfigurations;
+
+public class ApplicationStudentFileConfiguration : IEntityTypeConfiguration<ApplicationStudentFile>
+{
+    public void Configure(EntityTypeBuilder<ApplicationStudentFile> builder)
+    {
+        builder.ToTable("ApplicationsStudentFiles");
+
+        //Key compuesta
+        builder.HasKey(x => new { x.StudentFileId, x.ApplicationId });
+
+        //Relations
+        builder.HasOne(x => x.StudentFile)
+            .WithMany(x => x.ApplicationsFiles)
+            .HasForeignKey(x => x.StudentFileId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder.HasOne(x => x.Application)
+            .WithMany(x => x.Files)
+            .HasForeignKey(x => x.ApplicationId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
