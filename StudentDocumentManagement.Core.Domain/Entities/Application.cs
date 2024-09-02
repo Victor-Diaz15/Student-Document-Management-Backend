7d6712079a666e7c3475189587a636aef58b67bd
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-01 20:22:35 -0400

    Agregar entidades del sistema y los enums necesarios

---
+++ StudentDocumentManagement.Core.Domain/Entities/Application.cs
@@ -0,0 +1,13 @@
+﻿using StudentDocumentManagement.Core.Domain.Commons;
+using StudentDocumentManagement.Core.Domain.Enums;
+
+namespace StudentDocumentManagement.Core.Domain.Entities;
+
+public class Application : AuditableEntityBase
+{
+    public Guid StudentId { get; set; }
+    public Guid ServiceId { get; set; }
+    public Service Service { get; set; } = new();
+    public ApplicationStatus Status { get; set; }
+    public List<ApplicationStudentFile> Files { get; set; } = [];
+}
---
+++ StudentDocumentManagement.Core.Domain/Entities/ApplicationStudentFile.cs
@@ -0,0 +1,11 @@
+﻿using StudentDocumentManagement.Core.Domain.Commons;
+
+namespace StudentDocumentManagement.Core.Domain.Entities;
+
+public class ApplicationStudentFile : AuditableEntityBase
+{
+    public Guid ApplicationId { get; set; }
+    public Application Application { get; set; } = new();
+    public Guid StudentFileId { get; set; }
+    public StudentFile StudentFile { get; set; } = new();
+}
