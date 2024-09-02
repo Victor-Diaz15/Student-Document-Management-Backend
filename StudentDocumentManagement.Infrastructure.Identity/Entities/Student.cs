9eb0c07e5f379d323bc1cfe232de49a2170a05ff
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-01 21:31:01 -0400

    Eliminar listas de otras entidades

--- StudentDocumentManagement.Infrastructure.Identity/Entities/Student.cs
+++ StudentDocumentManagement.Infrastructure.Identity/Entities/Student.cs
@@ -5,7 +5,4 @@ namespace StudentDocumentManagement.Infrastructure.Identity.Entities;
 public class Student : UserApp
 {
     public string StudentId { get; set; } = string.Empty;
-    public List<StudentFile> Files { get; set; } = [];
-    public List<Application> Applications { get; set; } = [];
-
 }
