19048dc991b76a1a569ac8061f8a0e62cf122124
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-04 22:27:08 -0400

    Agregar entidades de los repos

---
+++ StudentDocumentManagement.Core.Domain/Abstractions/IStudentFileRepository.cs
@@ -0,0 +1,8 @@
+﻿using StudentDocumentManagement.Core.Domain.Entities;
+
+namespace StudentDocumentManagement.Core.Domain.Abstractions;
+
+public interface IStudentFileRepository : IGenericBaseRepository<StudentFile>
+{
+    Task<List<StudentFile>> GetAllStudentFilesAsync(Guid studentId);
+}
