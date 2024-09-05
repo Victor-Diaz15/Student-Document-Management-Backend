33bcf21d325c8299e56f235cbbf1a6422d9c220c
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-04 22:32:09 -0400

    Agregar funcionalidad de retornar un archivo por id

---
+++ StudentDocumentManagement.Core.Application/StudentFiles/Queries/GetStudentFileById/GetStudentFileByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using StudentDocumentManagement.Core.Application.Dtos.StudentFiles;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+
+namespace StudentDocumentManagement.Core.Application.StudentFiles.Queries.GetStudentFileById;
+
+public sealed record GetStudentFileByIdQuery(Guid Id) : IQuery<StudentFileDto>;
