4d359cb42b356f7cd62b0c5fb2dcfe9e4ccc5391
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-04 22:31:36 -0400

    Agregar funcionalidad de retornar el listado de archivos de un estudiante

---
+++ StudentDocumentManagement.Core.Application/StudentFiles/Queries/GetAllStudentFiles/GetAllStudentFilesQuery.cs
@@ -0,0 +1,6 @@
+﻿using StudentDocumentManagement.Core.Application.Dtos.StudentFiles;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+
+namespace StudentDocumentManagement.Core.Application.StudentFiles.Queries.GetAllStudentFiles;
+
+public sealed record GetAllStudentFilesQuery(Guid StudentId) : IQuery<List<StudentFileDto>>;
