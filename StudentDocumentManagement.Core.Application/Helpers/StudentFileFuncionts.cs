c017568f01a206182699836252782de48ac26cd9
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-04 22:29:16 -0400

    Agregar funcionalidades para el mapeo de student file type y file status

---
+++ StudentDocumentManagement.Core.Application/Helpers/StudentFileFuncionts.cs
@@ -0,0 +1,34 @@
+﻿using StudentDocumentManagement.Core.Domain.Enums;
+
+namespace StudentDocumentManagement.Core.Application.Helpers;
+
+public static class StudentFileFuncionts
+{
+    public static string GetFileType(StudentFileType? type)
+    {
+        string fileType = type switch
+        {
+            StudentFileType.ActaNacimiento => StudentFileType.ActaNacimiento.ToString(),
+            StudentFileType.CopiaCedula => StudentFileType.CopiaCedula.ToString(),
+            StudentFileType.FotocopiaTituloMaestria => StudentFileType.FotocopiaTituloMaestria.ToString(),
+            StudentFileType.DocumentoLegalizadoGrado => StudentFileType.DocumentoLegalizadoGrado.ToString(),
+            _ => ""
+        };
+
+        return fileType;
+    }
+
+    public static string GetFileStatus(FileStatus? status)
+    {
+        string fileStatus = status switch
+        {
+            FileStatus.Nuevo => FileStatus.Nuevo.ToString(),
+            FileStatus.Devuelto => FileStatus.Devuelto.ToString(),
+            FileStatus.Validado => FileStatus.Validado.ToString(),
+            FileStatus.Completado => FileStatus.Completado.ToString(),
+            _ => ""
+        };
+
+        return fileStatus;
+    }
+}
