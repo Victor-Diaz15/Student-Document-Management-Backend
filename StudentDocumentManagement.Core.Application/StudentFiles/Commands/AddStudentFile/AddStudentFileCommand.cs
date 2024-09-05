bef011884f893058d7f3ac4dd023b4d58110afcc
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-04 22:30:23 -0400

    Agregar funcionalidad de guardar un archivo

---
+++ StudentDocumentManagement.Core.Application/StudentFiles/Commands/AddStudentFile/AddStudentFileCommand.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+using StudentDocumentManagement.Core.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentDocumentManagement.Core.Application.StudentFiles.Commands.AddStudentFile;
+
+public sealed record AddStudentFileCommand(
+    [DataType(DataType.Upload)]
+    IFormFile File,
+    StudentFileType FileType
+    ) : ICommand<Guid>;
