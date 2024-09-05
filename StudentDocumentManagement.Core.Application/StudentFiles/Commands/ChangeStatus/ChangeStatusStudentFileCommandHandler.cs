f449ddeb1d29459361480ddced9235e45fc15a49
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-04 22:30:56 -0400

    Agregar funcionalidad de cambiar el estado de un archivo

---
+++ StudentDocumentManagement.Core.Application/StudentFiles/Commands/ChangeStatus/ChangeStatusStudentFileCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using StudentDocumentManagement.Core.Application.Interfaces;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+using StudentDocumentManagement.Core.Application.Shared.Results;
+using StudentDocumentManagement.Core.Domain.Abstractions;
+
+namespace StudentDocumentManagement.Core.Application.StudentFiles.Commands.ChangeStatus;
+
+internal class ChangeStatusStudentFileCommandHandler : ICommandHandler<ChangeStatusStudentFileCommand>
+{
+    private readonly IStudentFileRepository _studentFileRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ChangeStatusStudentFileCommandHandler(IStudentFileRepository studentFileRepository, IUnitOfWork unitOfWork)
+    {
+        _studentFileRepository = studentFileRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result> Handle(ChangeStatusStudentFileCommand request, CancellationToken cancellationToken)
+    {
+        var fileEntity = await _studentFileRepository.GetByIdAsync(request.FileId);
+
+        if (fileEntity == null)
+            return new Result(false, $"No existe archivo con el id: {request.FileId}");
+
+        fileEntity.Status = request.Status;
+
+        //guardando el registro
+        _studentFileRepository.UpdateEntity(fileEntity);
+
+        //persistir los cambios en la db
+        await _unitOfWork.SaveChangesAsync();
+
+        return new Result(true, "File Updated");
+    }
+}
