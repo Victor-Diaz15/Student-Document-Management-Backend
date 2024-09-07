7f88ffa605c9189dbc9bd5c47e6d541bf13a1b4b
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-06 23:16:37 -0400

    Agregar controller y endpoint de listar los servicios

---
+++ StudentDocumentManagement.Api/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StudentDocumentManagement.Core.Application.Services.Queries.GetAllServices;
+
+namespace StudentDocumentManagement.Api.Controllers;
+
+[Route("api/service")]
+public class ServiceController : ApiController
+{
+    public ServiceController(ISender sender) : base(sender)
+    {
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> GetAllServices()
+    {
+        var query = new GetAllServicesQuery();
+
+        var result = await Sender.Send(query);
+
+        return result.Success ? Ok(result) : BadRequest(result);
+    }
+}
