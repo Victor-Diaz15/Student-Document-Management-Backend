2db57caf56ca2adab37afc8f2375c1d268c3849a
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-06 23:14:36 -0400

    Agregar dto de service

---
+++ StudentDocumentManagement.Core.Application/Dtos/Services/ServiceDto.cs
@@ -0,0 +1,11 @@
+﻿using StudentDocumentManagement.Core.Domain.Enums;
+
+namespace StudentDocumentManagement.Core.Application.Dtos.Services;
+
+public class ServiceDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public string ServiceType { get; set; } = string.Empty;
+}
