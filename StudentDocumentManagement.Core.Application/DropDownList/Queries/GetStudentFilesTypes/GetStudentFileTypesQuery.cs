4e40bc81f8d01bfec170dcd3f29b9d4efe2e8284
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-06 23:16:14 -0400

    Agregar el use case de retornar los tipos de archivos para el dropdownlist

---
+++ StudentDocumentManagement.Core.Application/DropDownList/Queries/GetStudentFilesTypes/GetStudentFileTypesQuery.cs
@@ -0,0 +1,6 @@
+﻿using StudentDocumentManagement.Core.Application.Dtos.DropDownLists;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+
+namespace StudentDocumentManagement.Core.Application.DropDownList.Queries.GetStudentFilesTypes;
+
+public sealed record GetStudentFileTypesQuery : IQuery<List<DropDownListDto>>;
---
+++ StudentDocumentManagement.Core.Application/DropDownList/Queries/GetStudentFilesTypes/GetStudentFileTypesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using StudentDocumentManagement.Core.Application.Dtos.DropDownLists;
+using StudentDocumentManagement.Core.Application.Enums;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+using StudentDocumentManagement.Core.Application.Shared.Results;
+using StudentDocumentManagement.Core.Domain.Enums;
+using System.Text.RegularExpressions;
+
+namespace StudentDocumentManagement.Core.Application.DropDownList.Queries.GetStudentFilesTypes;
+
+internal class GetStudentFileTypesQueryHandler : IQueryHandler<GetStudentFileTypesQuery, List<DropDownListDto>>
+{
+    public Task<ResultT<List<DropDownListDto>>> Handle(GetStudentFileTypesQuery request, CancellationToken cancellationToken)
+    {
+        var fileTypes = Enum.GetValues(typeof(StudentFileType))
+                       .Cast<StudentFileType>()
+                       .Select(r => new DropDownListDto
+                       {
+                           Id = (int)r,
+                           //Este regex le agrega espacio entre mayusculas, toma las palabras asi (RoleEjemplo) y retorna el texto (Role Ejemplo)
+                           Name = Regex.Replace(r.ToString(), "(?<!^)([A-Z])", " $1")
+                       }).ToList();
+
+        var result = new ResultT<List<DropDownListDto>>(true, "Retrieving Student File Types", fileTypes);
+
+        return Task.FromResult(result);
+    }
+}
