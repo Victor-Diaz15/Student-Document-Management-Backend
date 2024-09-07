59624a5af9fd770147dafd09daeb1ca7b9d215ba
Victor-Diaz15 <victordiaz123julioarias@gmail.com>
2024-09-06 23:15:21 -0400

    Agregar use case de retornar el listado de servicios

---
+++ StudentDocumentManagement.Core.Application/Services/Queries/GetAllServices/GetAllServicesQuery.cs
@@ -0,0 +1,6 @@
+﻿using StudentDocumentManagement.Core.Application.Dtos.Services;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+
+namespace StudentDocumentManagement.Core.Application.Services.Queries.GetAllServices;
+
+public sealed record GetAllServicesQuery : IQuery<List<ServiceDto>>;
---
+++ StudentDocumentManagement.Core.Application/Services/Queries/GetAllServices/GetAllServicesQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using StudentDocumentManagement.Core.Application.Dtos.Services;
+using StudentDocumentManagement.Core.Application.Helpers;
+using StudentDocumentManagement.Core.Application.Interfaces.Messaging;
+using StudentDocumentManagement.Core.Application.Shared.Results;
+using StudentDocumentManagement.Core.Domain.Abstractions;
+using StudentDocumentManagement.Core.Domain.Entities;
+
+namespace StudentDocumentManagement.Core.Application.Services.Queries.GetAllServices;
+
+internal class GetAllServicesQueryHandler : IQueryHandler<GetAllServicesQuery, List<ServiceDto>>
+{
+    private readonly IGenericBaseRepository<Service> _serviceRepository;
+
+    public GetAllServicesQueryHandler(IGenericBaseRepository<Service> serviceRepository)
+    {
+        _serviceRepository = serviceRepository;
+    }
+
+    public async Task<ResultT<List<ServiceDto>>> Handle(GetAllServicesQuery request, CancellationToken cancellationToken)
+    {
+        var serviceListDb = await _serviceRepository.GetAllAsync();
+        var listDto = new List<ServiceDto>();
+
+        if (serviceListDb != null && serviceListDb.Count != 0)
+        {
+            foreach (var service in serviceListDb)
+            {
+                listDto.Add(new ServiceDto()
+                {
+                    Id = service.Id,
+                    Name = service.Name,
+                    Price = service.Price,
+                    ServiceType = ServiceTypeHelper.GetServiceType(service.ServiceType)
+                });
+            }
+
+            return new ResultT<List<ServiceDto>>(true, "Retrieving services of the system.", listDto);
+        }
+
+        return new ResultT<List<ServiceDto>>(false, "There are not services in the system.", null!);
+    }
+}
